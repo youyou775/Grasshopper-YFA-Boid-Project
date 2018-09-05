b657daffa80aba8dba90d76037803b248ee1f42e
youyou775 <42960056+youyou775@users.noreply.github.com>
2018-09-05 14:20:26 +0200

    Add files via upload

---
+++ SurfaceTrails2/FlockingMapToSurface/GhcFlockingSimulation.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace SurfaceTrails2.FlockingMapToSurface
+{
+    public class GhcFlockingSimulation : GH_Component
+    {
+        private FlockSystem flockSystem;
+
+        public GhcFlockingSimulation()
+            : base(
+                  "Flocking Map To Surface",
+                  "FlockingMapToSurface",
+                  "Flocking Map To Surface",
+                  "YFAtools",
+                  "AgentBased")
+        {
+        }
+
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Reset", "Reset", "Reset", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("Play", "Play", "Play", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("3D", "3D", "3D", GH_ParamAccess.item, true);
+            pManager.AddSurfaceParameter("srf", "srf", "srf", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Count", "Count", "Number of Agents", GH_ParamAccess.item, 50);
+            pManager.AddNumberParameter("Timestep", "Timestep", "Timestep", GH_ParamAccess.item, 0.02);
+            pManager.AddNumberParameter("Neighbourhood Radius", "Neighbourhood Radius", "Neighbourhood Radius", GH_ParamAccess.item, 3.5);
+            pManager.AddNumberParameter("Alignment", "Alignment", "Alignment", GH_ParamAccess.item, 0.5);
+            pManager.AddNumberParameter("Cohesion", "Cohesion", "Cohesion", GH_ParamAccess.item, 0.5);
+            pManager.AddNumberParameter("Separation", "Separation", "Separation", GH_ParamAccess.item, 0.5);
+            pManager.AddNumberParameter("Separation Distance", "Separation Distance", "Separation Distance", GH_ParamAccess.item, 1.5);
+            pManager.AddCircleParameter("Repellers", "Repellers", "Repellers", GH_ParamAccess.list);
+            pManager[10].Optional = true;
+            pManager.AddBooleanParameter("Use Parallel", "Use Parallel", "Use Parallel", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("Use R-Tree", "Use R-Tree", "Use R-Tree", GH_ParamAccess.item, false);
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Info", "Info", "Information", GH_ParamAccess.item);
+            pManager.AddPointParameter("Positions", "Positions", "The agent positions", GH_ParamAccess.list);
+            pManager.AddVectorParameter("Velocities", "Velocities", "The agent veloctiies", GH_ParamAccess.list);
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // ===============================================================================================
+            // Read input parameters
+            // ===============================================================================================
+
+            bool iReset = true;
+            bool iPlay = false;
+            bool i3D = false;
+            Surface baseSurface = null;
+            int iCount = 0;
+            double iTimestep = 0.0;
+            double iNeighbourhoodRadius = 0.0;
+            double iAlignment = 0.0;
+            double iCohesion = 0.0;
+            double iSeparation = 0.0;
+            double iSeparationDistance = 0.0;
+            List<Circle> iRepellers = new List<Circle>();
+            bool iUseParallel = false;
+            bool iUseRTree = false;
+
+            DA.GetData("Reset", ref iReset);
+            DA.GetData("Play", ref iPlay);
+            DA.GetData("3D", ref i3D);
+            DA.GetData("srf", ref baseSurface);
+            DA.GetData("Count", ref iCount);
+            DA.GetData("Timestep", ref iTimestep);
+            DA.GetData("Neighbourhood Radius", ref iNeighbourhoodRadius);
+            DA.GetData("Alignment", ref iAlignment);
+            DA.GetData("Cohesion", ref iCohesion);
+            DA.GetData("Separation", ref iSeparation);
+            DA.GetData("Separation Distance", ref iSeparationDistance);
+            DA.GetDataList("Repellers", iRepellers);
+            DA.GetData("Use Parallel", ref iUseParallel);
+            DA.GetData("Use R-Tree", ref iUseRTree);
+
+
+            // ===============================================================================================
+            // Read input parameters
+            // ===============================================================================================
+
+            if (iReset || flockSystem == null)
+            {
+                flockSystem = new FlockSystem(iCount, i3D);
+            }
+            else
+            {
+                // ===============================================================================================
+                // Assign the input parameters to the corresponding variables in the  "flockSystem" object
+                // ===============================================================================================
+
+                flockSystem.Timestep = iTimestep;
+                flockSystem.NeighbourhoodRadius = iNeighbourhoodRadius;
+                flockSystem.AlignmentStrength = iAlignment;
+                flockSystem.CohesionStrength = iCohesion;
+                flockSystem.SeparationStrength = iSeparation;
+                flockSystem.SeparationDistance = iSeparationDistance;
+                flockSystem.Repellers = iRepellers;
+                flockSystem.UseParallel = iUseParallel;
+
+
+                // ===============================================================================
+                // Update the flock
+                // ===============================================================================
+
+                if (iUseRTree)
+                    flockSystem.UpdateUsingRTree();
+                else
+                    flockSystem.Update();
+
+                if (iPlay) ExpireSolution(true);
+            }
+
+            // ===============================================================================
+            // Output the agent positions and velocities so we can see them on display
+            // ===============================================================================
+
+            List<GH_Point> positions = new List<GH_Point>();
+            List<GH_Vector> velocities = new List<GH_Vector>();
+            List<GH_Point> surfacePositions = new List<GH_Point>();
+
+            var surface = baseSurface.ToNurbsSurface();
+            foreach (FlockAgent agent in flockSystem.Agents)
+            {
+                positions.Add(new GH_Point(agent.Position));
+                velocities.Add(new GH_Vector(agent.Velocity));
+
+
+                // ===============================================================================
+                // Position on surface 
+                // ===============================================================================
+                Interval u = new Interval(0, 1);
+                Interval v = new Interval(0, 1);
+
+                surface.SetDomain(0, u);
+                surface.SetDomain(1, v);
+
+
+
+
+                var nu = (agent.Position.X - 0) / (flockSystem.BoundingBox - 0);
+                var nv = (agent.Position.Y - 0) / (flockSystem.BoundingBox - 0);
+
+
+                surfacePositions.Add(new GH_Point(surface.PointAt(nu, nv))); 
+
+            }
+
+           
+
+            DA.SetDataList("Positions", surfacePositions);
+            DA.SetDataList("Velocities", velocities);
+        }
+
+
+        protected override System.Drawing.Bitmap Icon { get { return Properties.Resources._28_8_18_FlockSimulation; } }
+
+
+        public override Guid ComponentGuid { get { return new Guid("7066d2ac-3c0a-47db-b22f-4d8c25bf8baa"); } }
+    }
+}
---
+++ SurfaceTrails2/FlockingOnSrf/FlockSystem.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace SurfaceTrails2.FlockingOnSrf
+{
+    public class FlockSystem
+    {
+        public List<FlockAgent> Agents;
+
+        public double Timestep;
+        public double NeighbourhoodRadius;
+        public double AlignmentStrength;
+        public double CohesionStrength;
+        public double SeparationStrength;
+        public double SeparationDistance;
+        public List<Circle> Repellers;
+        public bool UseParallel;
+         NurbsSurface _surface;
+        private double _boundingBoxSize = 1;
+        private Random _randomX = new Random();
+        private Random _randomY = new Random();
+
+
+        public double BoundingBox { get { return _boundingBoxSize; } set { _boundingBoxSize = value; } }
+        public NurbsSurface Surface { get { return _surface; } set { _surface = value; } }
+
+
+        public FlockSystem(int agentCount/*, bool is3D*/,NurbsSurface surface)
+        {
+            Surface = surface;
+            Agents = new List<FlockAgent>();
+            Random random = new Random();
+            Interval domainU = _surface.Domain(0);
+            Interval domainV = _surface.Domain(1);
+
+   
+
+
+            _surface.SetDomain(0, domainU);
+            _surface.SetDomain(0, domainV);
+
+            double randomCoordinateX = _randomX.NextDouble();
+            double randomCoordinateY = _randomX.NextDouble();
+
+          
+
+            var tu = domainU.ParameterAt(random.NextDouble());
+            var tv = domainV.ParameterAt(random.NextDouble());
+            //var vectorX = _surface.PointAt(Util.GetRandomUnitVectorX(), Util.GetRandomUnitVectorY()).X;
+            //var vectory = _surface.PointAt(Util.GetRandomUnitVectorX(), Util.GetRandomUnitVectorY()).Y;
+
+            var vectorX = _surface.PointAt(Util.GetRandomUnitVectorX(), Util.GetRandomUnitVectorY()).X;
+            var vectory = _surface.PointAt(Util.GetRandomUnitVectorX(), Util.GetRandomUnitVectorY()).Y;
+
+
+            //if (is3D)
+            //    for (int i = 0; i < agentCount; i++)
+            //    {
+            //        FlockAgent agent = new FlockAgent(
+            //            Util.GetRandomPoint(0.0, 30.0, 0.0, 30.0, 0.0, 30.0),
+            //            Util.GetRandomUnitVector() * 4.0);
+
+            //        agent.FlockSystem = this;
+
+            //        Agents.Add(agent);
+            //    }
+            //else
+            for (int i = 0; i < agentCount; i++)
+                {
+                    FlockAgent agent = new FlockAgent(
+                        _surface.PointAt(tu, tv),new Vector3d(Point3d.Subtract(_surface.PointAt(tu, tv), 
+                            _surface.PointAt(tu/(_surface.Domain(0).T1-_surface.Domain(0).T0),tv/ (_surface.Domain(1).T1 - _surface.Domain(0).T1)))) );
+
+
+                agent.FlockSystem = this;
+
+                    Agents.Add(agent);
+                }
+        }
+
+
+        private List<FlockAgent> FindNeighbours(FlockAgent agent)
+        {
+            var neighbours = new List<FlockAgent>();
+
+            foreach (FlockAgent neighbour in Agents)
+                if (neighbour != agent && neighbour.Position.DistanceTo(agent.Position) < NeighbourhoodRadius)
+                    neighbours.Add(neighbour);
+
+            return neighbours;
+        }
+
+        private void ComputeAgentDesiredVelocity(FlockAgent agent)
+        {
+            List<FlockAgent> neighbours = FindNeighbours(agent);
+            agent.ComputeDesiredVelocity(neighbours);
+        }
+
+
+        public void Update()
+        {
+            if (UseParallel)
+                Parallel.ForEach(Agents, ComputeAgentDesiredVelocity);
+            else
+                foreach (var agent in Agents)
+                    ComputeAgentDesiredVelocity(agent);
+
+            // Once the desired velocity for each agent has been computed, we update each position and velocity
+            foreach (var agent in Agents)
+                agent.UpdateVelocityAndPosition();
+        }
+
+
+        public void UpdateUsingRTree()
+        {
+            /* First, build the R-Tree */
+
+            var rTree = new RTree();
+
+            for (var i = 0; i < Agents.Count; i++)
+                rTree.Insert(Agents[i].Position, i);
+
+            /* Then, we use the R-Tree to find the neighbours
+                and compute the desired velocity */
+
+            foreach (var agent in Agents)
+            {
+                List<FlockAgent> neighbours = new List<FlockAgent>();
+
+                EventHandler<RTreeEventArgs> rTreeCallback =
+                (object sender, RTreeEventArgs args) =>
+                {
+                    if (Agents[args.Id] != agent)
+                        neighbours.Add(Agents[args.Id]);
+                };
+
+                rTree.Search(new Sphere(agent.Position, NeighbourhoodRadius), rTreeCallback);
+
+                agent.ComputeDesiredVelocity(neighbours);
+            }
+
+
+
+            // Once the desired velocity for each agent has been computed, we can update each position and velocity               
+            foreach (FlockAgent agent in Agents) agent.UpdateVelocityAndPosition();
+        }
+    }
+}
